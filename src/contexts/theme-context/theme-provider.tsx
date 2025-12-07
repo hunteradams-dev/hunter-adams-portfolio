92b0fa8baab13ac22e8a15e28dffb94bf598663f
Hunter Adams <hunteradams10@gmail.com>
2025-12-07 21:08:22 +1100

    separated types in theme context and created some string consts

--- src/contexts/theme-context/theme-provider.tsx
+++ src/contexts/theme-context/theme-provider.tsx
@@ -1,26 +1,21 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "../styles/global-style";
-import { darkTheme, lightTheme } from "../styles/theme";
-
-export enum Themes {
-  Light = "light",
-  Dark = "dark",
-}
-
-type ThemeContextValue = {
-  theme: Themes;
-  toggleTheme: () => void;
-};
-
-type ThemeProviderProps = { children: React.ReactNode };
+import { GlobalStyle } from "../../styles/global-style";
+import { darkTheme, lightTheme } from "../../styles/theme";
+import { CHANGE, THEME, ThemeContextValue, ThemeProviderProps, Themes } from "./utils";
 
 const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
 
 const ThemeProviderComponent = ({ children }: ThemeProviderProps) => {
   const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
   const [theme, setTheme] = useState<Themes>(() => {
-    const savedMode = localStorage.getItem("theme") as Themes;
+    const savedMode = localStorage.getItem(THEME) as Themes;
     return savedMode
       ? (savedMode as Themes)
       : prefersDarkMode.matches
@@ -29,19 +24,19 @@ const ThemeProviderComponent = ({ children }: ThemeProviderProps) => {
   });
 
   const toggleTheme = () => {
-    const newTheme = theme === Themes.Light ? Themes.Dark : Themes.Light
+    const newTheme = theme === Themes.Light ? Themes.Dark : Themes.Light;
     setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
+    localStorage.setItem(THEME, newTheme);
   };
 
   useEffect(() => {
     const handleChange = () => {
       setTheme(prefersDarkMode.matches ? Themes.Dark : Themes.Light);
     };
-    prefersDarkMode.addEventListener("change", handleChange);
+    prefersDarkMode.addEventListener(CHANGE, handleChange);
 
     return () => {
-      prefersDarkMode.removeEventListener("change", handleChange);
+      prefersDarkMode.removeEventListener(CHANGE, handleChange);
     };
   }, [prefersDarkMode]);
 
